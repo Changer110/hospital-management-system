33416d224aad0125407df5dc0b7fc7fc70fea38e
Bill <131465945+RgixSys@users.noreply.github.com>
2024-03-15 08:13:55 +0200

    Update model user

--- hospital/App/models/user.py
+++ hospital/App/models/user.py
@@ -4,5 +4,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    roles = [('Doctor','Doctor')]
-    role = models.CharField(max_length=10, choices=roles, default = 'Doctor', null=False)
+    roles = [
+        ('Admin','Admin'),
+        ('Doctor','Doctor'),
+        ('Nurse','Nurse')]
+    role = models.CharField(max_length=10, choices=roles, default = 'Admin', null=False)
